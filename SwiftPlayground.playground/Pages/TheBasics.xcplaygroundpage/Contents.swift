af2352c19dec459185c81d7df426f698fd514367
loves9 <ifengfree@gmail.com>
2019-04-18 15:27:19 +0800

    [+]basics

--- SwiftPlayground.playground/Pages/TheBasics.xcplaygroundpage/Contents.swift
+++ SwiftPlayground.playground/Pages/TheBasics.xcplaygroundpage/Contents.swift
@@ -2,6 +2,32 @@
 
 import Foundation
 
-var str = "Hello, playground"
+
+// 一、Constants and Variables
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+var welcomeMessage: String
+welcomeMessage = "Hello"
+
+var friendlyWelcome = "Hello!"
+friendlyWelcome = "Bonjour!"
+
+//print("The current value of friendlyWelcome is \(friendlyWelcome)")
+
+// 二、Comments
+
+
+// 三、Semicolons
+
+// 四、Integers
+
+let minValue = UInt8.min  // minValue is equal to 0, and is of type UInt8
+let maxValue = UInt8.max  // maxValue is equal to 255, and is of type UInt8
+
+let pi = 3.14159
+type(of: pi)
+
+
 
 //: [Next](@next)
